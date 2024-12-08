a12161e2faaae34abc3e0b645931476163cfb044
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2024-12-08 17:33:23 +0200

    change the structure of the services

--- BlogSystem.Infrastructure/Data/Configurations/ApplicationUserConfigurations.cs
+++ BlogSystem.Infrastructure/Data/Configurations/ApplicationUserConfigurations.cs
@@ -11,33 +11,33 @@ using System.Threading.Tasks;
 
 namespace BlogSystem.Infrastructure.Data.Configurations
 {
-	public class ApplicationUserConfigurations : IEntityTypeConfiguration<ApplicationUser>
-	{
-		public void Configure(EntityTypeBuilder<ApplicationUser> builder)
-		{
-			builder.OwnsMany(x => x.RefreshTokens).ToTable("RefreshTokens").WithOwner().HasForeignKey("UserId");
+    public class ApplicationUserConfigurations : IEntityTypeConfiguration<ApplicationUser>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+        {
+            builder.OwnsMany(x => x.RefreshTokens).ToTable("RefreshTokens").WithOwner().HasForeignKey("UserId");
 
-			builder.Property(x => x.FirstName).HasMaxLength(100);
-			builder.Property(x => x.LastName).HasMaxLength(100);
+            builder.Property(x => x.FirstName).HasMaxLength(100).IsRequired();
+            builder.Property(x => x.LastName).HasMaxLength(100).IsRequired();
 
-			//Default Data
+            //Default Data
 
-			var passwordHasher = new PasswordHasher<ApplicationUser>();
+            var passwordHasher = new PasswordHasher<ApplicationUser>();
 
-			builder.HasData(new ApplicationUser
-			{
-				Id = DefaultUsers.AdminId,
-				FirstName = "Blog System",
-				LastName = "Admin",
-				UserName = DefaultUsers.AdminEmail,
-				NormalizedUserName = DefaultUsers.AdminEmail.ToUpper(),
-				Email = DefaultUsers.AdminEmail,
-				NormalizedEmail = DefaultUsers.AdminEmail.ToUpper(),
-				SecurityStamp = DefaultUsers.AdminSecurityStamp,
-				ConcurrencyStamp = DefaultUsers.AdminConcurrencyStamp,
-				EmailConfirmed = true,
-				PasswordHash = passwordHasher.HashPassword(null!, DefaultUsers.AdminPassword)
-			});
-		}
-	}
+            builder.HasData(new ApplicationUser
+            {
+                Id = DefaultUsers.AdminId,
+                FirstName = "Blog System",
+                LastName = "Admin",
+                UserName = DefaultUsers.AdminEmail,
+                NormalizedUserName = DefaultUsers.AdminEmail.ToUpper(),
+                Email = DefaultUsers.AdminEmail,
+                NormalizedEmail = DefaultUsers.AdminEmail.ToUpper(),
+                SecurityStamp = DefaultUsers.AdminSecurityStamp,
+                ConcurrencyStamp = DefaultUsers.AdminConcurrencyStamp,
+                EmailConfirmed = true,
+                PasswordHash = passwordHasher.HashPassword(null!, DefaultUsers.AdminPassword)
+            });
+        }
+    }
 }
