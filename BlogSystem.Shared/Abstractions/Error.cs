503da3a625490aff3fe21308de597bae66bc75ca
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2024-12-21 00:22:23 +0200

    Add Error and Result classes

---
+++ BlogSystem.Shared/Abstractions/Error.cs
@@ -0,0 +1,7 @@
+﻿namespace BlogSystem.Shared.Abstractions
+{
+    public record Error(string code, string Description)
+    {
+        public static readonly Error None = new(string.Empty, string.Empty);
+    }
+}
