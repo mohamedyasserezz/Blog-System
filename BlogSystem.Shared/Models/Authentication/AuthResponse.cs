35782ca13d40b37e32797bddd4dd76069c76c6fa
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-12-20 22:24:32 +0200

    add login and register implementation

---
+++ BlogSystem.Shared/Models/Authentication/AuthResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogSystem.Shared.Models.Authentication
+{
+	public class AuthResponse
+	(
+		string Id,
+		string? Email,
+		string? FirstName,
+		string? LastName,
+		string Token,
+		int ExpiresIn,
+		string RefreshToken,
+		DateTime RefreshTokenExpiration
+	);
+}
---
+++ BlogSystem.Shared/Models/Authentication/RegisterRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogSystem.Shared.Models.Authentication
+{
+	public record RegisterRequest(
+		string Email,
+		string Password,
+		string FirstName,
+		string LastName
+	);
+}
