92957a8765a8e75b850f4f7b4a470fd7bfa4af60
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-12-21 20:48:20 +0200

    add error for user and add status code for post error

--- BlogSystem.Shared/_Common/Errors/PostErrors.cs
+++ BlogSystem.Shared/_Common/Errors/PostErrors.cs
@@ -1,10 +1,11 @@
 ﻿using BlogSystem.Shared.Abstractions;
+using Microsoft.AspNetCore.Http;
 
 namespace BlogSystem.Shared.Common.Errors
 {
     public static class PostErrors
     {
         public static Error PostNotFound =
-        new("Post.NotFound", "No post was found with the given id");
+        new("Post.NotFound", "No post was found with the given id", StatusCodes.Status404NotFound);
     }
 }
