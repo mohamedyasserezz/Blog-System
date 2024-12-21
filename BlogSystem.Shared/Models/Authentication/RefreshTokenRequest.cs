3e124c148329c1f2403f7f7f258d9a6662e151fc
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-12-21 20:48:31 +0200

    add refresh token request

---
+++ BlogSystem.Shared/Models/Authentication/RefreshTokenRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogSystem.Shared.Models.Authentication
+{
+	public record RefreshTokenRequest
+	(
+		string Token,
+		string RefreshToken
+	);
+}
