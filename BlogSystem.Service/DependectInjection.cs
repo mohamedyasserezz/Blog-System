dae6fdc2f5ccf1832f700300b5f1a33b8c5c78c5
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-12-23 22:24:23 +0200

    add email service for sending email and hangfire library

--- BlogSystem.Service/DependectInjection.cs
+++ BlogSystem.Service/DependectInjection.cs
@@ -1,7 +1,10 @@
 ﻿using BlogSystem.Domain.Contract.Posts;
 using BlogSystem.Domain.Contract.Services.Authentication;
+using BlogSystem.Service.Authentication;
 using BlogSystem.Service.Mapping;
 using BlogSystem.Service.Posts;
+using Hangfire;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,9 +15,20 @@ namespace BlogSystem.Service
         public static IServiceCollection AddServicesDependency(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddScoped<IPostService, PostService>();
+            services.AddScoped<IAuthService, AuthService>();
+            services.AddScoped<IEmailSender, EmailService>();
+
             services.AddAutoMapper(typeof(MappingProfile));
 
-            return services;
+			services.AddHangfire(config => config
+					.SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+					.UseSimpleAssemblyNameTypeSerializer()
+					.UseRecommendedSerializerSettings()
+					.UseSqlServerStorage(configuration.GetConnectionString("HangfireConnection")));
+
+			services.AddHangfireServer();
+
+			return services;
         }
     }
 }
---
+++ BlogSystem.Service/Settings/MailSettings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogSystem.Service.Settings
+{
+	public class MailSettings
+	{
+		[Required, EmailAddress]
+		public string Mail { get; set; } = string.Empty;
+		[Required]
+		public string DisplayName { get; set; } = string.Empty;
+		[Required]
+		public string Password { get; set; } = string.Empty;
+		[Required]
+		public string Host { get; set; } = string.Empty;
+		[Range(100, 999)]
+		public int Port { get; set; }
+	}
+}
