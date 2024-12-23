8b8a841265465c41a371ff29d2bc40ea8c4927a4
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-12-23 22:21:51 +0200

    add forget password request and its validation

---
+++ BlogSystem.Shared/Models/Authentication/ForgetPasswrodRequestValidation.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogSystem.Shared.Models.Authentication
+{
+	public class ForgetPasswrodRequestValidation : AbstractValidator<ForgetPasswrodRequest>
+	{
+        public ForgetPasswrodRequestValidation()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .EmailAddress();
+        }
+    }
+}
