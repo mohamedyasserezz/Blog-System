3d054ffd836f688c0ab482c42ee9710830e21219
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2024-12-23 22:45:23 +0200

    Add implementation Get Post  for Post service

--- BlogSystem.Domain/Contract/Services/Posts/IPostService.cs
+++ BlogSystem.Domain/Contract/Services/Posts/IPostService.cs
@@ -6,7 +6,7 @@ namespace BlogSystem.Domain.Contract.Posts
     public interface IPostService
     {
         //Task<Result<IEnumerable<PostResponse>>> GetAllAsync(CancellationToken cancellationToken = default);
-        //Task<Result<PostResponse>> GetAsync(int id, CancellationToken cancellationToken = default);
+        Task<Result<PostResponse>> GetAsync(int id, CancellationToken cancellationToken = default);
         Task<Result<PostResponse>> CreatePostAsync(PostRequest postRequest, CancellationToken cancellationToken = default);
         //Task<Result> UpdateAsync(int id, PostRequest poll, CancellationToken cancellationToken = default);
         //Task<Result> DeleteAsync(int id, CancellationToken cancellationToken = default);
--- BlogSystem.Service/Posts/PostService.cs
+++ BlogSystem.Service/Posts/PostService.cs
@@ -36,5 +36,17 @@ namespace BlogSystem.Service.Posts
 
             return Result.Success(postResponse);
         }
+
+        public async Task<Result<PostResponse>> GetAsync(int id, CancellationToken cancellationToken = default)
+        {
+            var post = await _unitOfWork.GetRepository<Post>().GetByIdAsync(id);
+
+            if(post is null)
+                return Result.Failer<PostResponse>(PostErrors.PostNotFound);
+
+            var postResponse = _mapper.Map<PostResponse>(post);
+
+            return Result.Success(postResponse);
+        }
     }
 }
