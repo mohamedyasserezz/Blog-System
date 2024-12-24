4671e0e11834fe9d56d56201ab3260b19e0047ae
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2024-12-24 02:41:00 +0200

    Add implementation for GetAll on Post service

--- BlogSystem.Service/Posts/PostService.cs
+++ BlogSystem.Service/Posts/PostService.cs
@@ -1,20 +1,37 @@
 ﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using BlogSystem.Domain.Contract.Infrastructure;
 using BlogSystem.Domain.Contract.Posts;
 using BlogSystem.Domain.Enities;
+using BlogSystem.Infrastructure.Data;
 using BlogSystem.Shared.Abstractions;
 using BlogSystem.Shared.Common.Errors;
+using BlogSystem.Shared.Models.Common;
 using BlogSystem.Shared.Models.Posts;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlogSystem.Service.Posts
 {
     internal class PostService(
         IUnitOfWork unitOfWork,
+        ApplicationDbContext applicationDbContext,
         IMapper mapper
         ) : IPostService
     {
         private readonly IUnitOfWork _unitOfWork = unitOfWork;
+        private readonly ApplicationDbContext _applicationDbContext = applicationDbContext;
         private readonly IMapper _mapper = mapper;
+
+        public async Task<Result<Pagination<PostResponse>>> GetAllAsync(RequestFilter requestFilter,CancellationToken cancellationToken = default)
+        {
+            var source = _applicationDbContext.Posts
+                        .Include(x => x.Tags)
+                        .ProjectTo<PostResponse>(_mapper.ConfigurationProvider)
+                        .AsNoTracking();
+            var posts = await Pagination<PostResponse>.CreateAsync(source, requestFilter.PageNumber, requestFilter.PageSize, cancellationToken);
+
+            return Result.Success(posts);
+        }
 
         public async Task<Result<PostResponse>> GetAsync(int id, CancellationToken cancellationToken = default)
         {
@@ -78,5 +95,7 @@ namespace BlogSystem.Service.Posts
             return Result.Success();
 
         }
+
+
     }
 }
